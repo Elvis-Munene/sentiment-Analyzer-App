53d70f855a710ee74480eef81b3596cdbbb322c6
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-25 09:27:00 +0300

    adds a fetch function to get user data

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ const inputForm = document.querySelector('form');
 inputForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     const input = document.querySelector('textarea#comment')
-    console.log(input.value)
+    console.log(input.value);
+    fetch(`http://localhost:3000/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+})
+
 })
 
 
